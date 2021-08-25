86fe8a6f3a977c3cc1a9000e609c9ed242264bae
JaveedBeargrylls <76989549+JaveedBeargrylls@users.noreply.github.com>
2021-08-25 20:30:27 +0530

    [Javeed] Add : initial commit - WelcomeNote

---
+++ employeeWage.py
@@ -0,0 +1,19 @@
+'''
+@Author: Javeed
+@Date: 2021-08-25
+@Last Modified by: Javeed
+@Last Modified time: 2021-08-25 19:36:20
+@Title : Program Aim
+'''
+
+def emply_Wage():
+    """
+    Description:
+        Function description to print the ' Welcome Message ' 
+    Return:
+        List of variable values are returning from function
+    """
+    print ('Welcome to the Employee Wage Computation')
+    
+if __name__ == '__main__':
+    emply_Wage()
